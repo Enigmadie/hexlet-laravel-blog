8dd216a67f622f30ca70bdf508f10b8ea4c2ea60
Enigmadie <gagen666@mail.ru>
2019-11-29 09:49:25 +0300

    add resource route

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach($articles as $article)
-        <a href="{{route('articles.show', ['id' => $article->id])}}">{{$article->name}}</a>
-        <a href="{{ route('articles.edit', ['id' => $article->id]) }}">Edit</a>
-        <a href="{{ route('articles.destroy', ['id' => $article->id]) }}" data-method="delete" rel="nofollow">Delete</a>
+        <a href="{{route('articles.show', $article->id)}}">{{$article->name}}</a>
+        <a href="{{ route('articles.edit', $article->id) }}">Edit</a>
+        <a href="{{ route('articles.destroy', $article->id) }}" data-method="delete" rel="nofollow">Delete</a>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,4 @@
 
 Route::get('/', 'PageController@about' );
 Route::get('/about', 'PageController@about' );
-Route::get('/articles', 'ArticleController@index')->name('articles.index');
-Route::get('/articles/create', 'ArticleController@create')->name('articles.create');
-Route::delete('/articles/{id}', 'ArticleController@destroy')
-  ->name('articles.destroy');
-Route::get('/articles/{id}', 'ArticleController@show')
-  ->name('articles.show');
-Route::post('/articles', 'ArticleController@store')
-  ->name('articles.store');
-Route::get('/articles/{id}/edit', 'ArticleController@edit')
-  ->name('articles.edit');
-Route::patch('/articles/{id}', 'ArticleController@update')
-  ->name('articles.update');
-
+Route::resource('/articles', 'ArticleController')->names('articles');
